fa5131a19ee4c19d120191048dda5a27720e9236
Boris Ts. (cborka) <cborka777@yandex.ru>
2019-11-22 17:11:08 +0700

    2019-11-22 осваиваю Material UI

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 import Button from "@material-ui/core/Button";
 import * as axios from "axios";
 import ButtonX from "./components/ButtonX/ButtonX";
-import MenuAppBar from './components/Header/Header';
+import Header from './components/Header/Header';
 import TableList from './components/TableList/TableList'
+import Container from '@material-ui/core/Container';
+import Typography from "@material-ui/core/Typography";
+import Footer from "./components/Footer/Footer";
+import {makeStyles} from '@material-ui/core/styles';
+import {purple} from "@material-ui/core/colors";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        color: purple[900],
+        background: purple[50],
+        padding: 50
+    }
+}));
+
 
 const onButtonClick = () => {
     axios.get(`http://localhost:3003/`).then((response) => {
@@ -13,9 +27,13 @@ const onButtonClick = () => {
 };
 
 function App() {
+    const classes = useStyles();
     return (
-        <>
-            <MenuAppBar/>
+        <Container maxWidth="lg" className={classes.root} >
+
+            <Header />
+
+
 
             <div className='padding30'>
                 "это кнопка"
@@ -34,10 +52,12 @@ function App() {
                     Это тоже кнопка
                 </Button>
 
-                <TableList/>
+             {/* <TableList/>*/}
 
             </div>
-        </>
+
+            <Footer />
+        </Container>
     );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,14 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import {cyan, deepPurple} from "@material-ui/core/colors";
+
 
 const useStyles = makeStyles(theme => ({
     root: {
-        flexGrow: 1,
+       flexGrow: 2,
+       padding: 0,
+        background: cyan[300]
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -17,14 +21,17 @@ const useStyles = makeStyles(theme => ({
     title: {
         flexGrow: 1,
     },
+    back: {
+        background: cyan[500]
+    }
 }));
 
-export default function MenuAppBar() {
+export default function Header() {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <AppBar position="static">
+            <AppBar position="static"  className={classes.back} >
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
